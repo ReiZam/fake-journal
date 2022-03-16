01bb2e0fba4d5764f4f5aaabebac0a63939cc981
ReiZam <kmazier@student.42.fr>
2022-03-16 02:22:10 +0100

    Clean-up Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+function FakeJournalApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default FakeJournalApp
