79e817b5c2ede28693cfb8fa8266b5c388050fd3
ReiZam <kmazier@student.42.fr>
2022-03-16 07:32:14 +0100

    add footer

--- components/layout/footer.layout.component.js
+++ components/layout/footer.layout.component.js
@@ -2,8 +2,12 @@ function Footer()
 {
 	return (
 		<footer className="relative py-6 bg-white">
-			<div className="container flex">
-				
+			<div className="container flex-col mx-auto flex">
+				<div className="flex flex-row justify-between">
+					<p>Articles come from <a href="https://newsapi.org/">newsapi.org</a></p>
+					<p>© 2022 fake journal.</p>
+				</div>
+				<a href="https://github.com/ReiZam">Made by K. MAZIER</a>
 			</div>
 		</footer>
 	);
