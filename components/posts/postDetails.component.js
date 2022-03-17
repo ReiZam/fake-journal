283062f62a1afe2580024a078380095fd526b1d5
kmazier <mazier.karl@gmail.com>
2022-03-17 03:04:04 +0000

    postDetails modal are now scrollable when the content is too big

--- components/posts/postDetails.component.js
+++ components/posts/postDetails.component.js
@@ -1,7 +1,7 @@
 function PostDetails({post, closePost})
 {
 	return (
-		<div className="flex justify-between flex-col p-4 space-y-4">
+		<div className="flex justify-between flex-col p-4 space-y-4 max-h-[85vh] overflow-y-auto">
 			<img className="object-contain rounded-lg" src={post.urlToImage}/>
 			<p className="text-xl font-medium">{post.title}</p>
 			<p className="text-sm">{post.description}...</p>
